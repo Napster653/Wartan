d12574748c4d169be23453741d5a473bcc0b2355
Pablo Ferrer <100330656@alumnos.uc3m.es>
2022-09-22 13:41:28 +0200

    2022-09-23 v1

--- script.js
+++ script.js
@@ -1,37 +1,48 @@
 const allTds = Array.from(document.getElementsByTagName("td"));
-for (i in allTds) {
-    if (allTds[i].matches("table tr td table tr td")) {
+for (i in allTds)
+{
+    if (allTds[i].matches("table tr td table tr td") && !allTds[i].matches(".disabled tr td"))
+    {
         allTds[i].onmouseover = function (e) { highlightThis(e) };
         allTds[i].onmouseout = function () { unHighlightAll() };
     }
 }
 
-function highlightThis(e) {
+function highlightThis(e)
+{
     e = e || window.event;
     var target = e.target || e.srcElement;
 
     var malla;
 
-    if (target.innerHTML.includes("PRC343-")) {
-        for (let i = 0; i < allTds.length; i++) {
-            if (allTds[i].innerHTML == target.innerHTML) {
+    if (target.innerHTML.includes("PRC343-"))
+    {
+        for (let i = 0; i < allTds.length; i++)
+        {
+            if (allTds[i].innerHTML == target.innerHTML && !allTds[i].matches(".disabled tr td"))
+            {
                 allTds[i].style.backgroundColor = "#E1BE6A";
                 allTds[i].parentElement.parentElement.parentElement.parentElement.style.backgroundColor = "#E1BE6A";
             }
         }
     }
-    else if (target.innerHTML.includes("PRC117-") || target.innerHTML.includes("PRC152-")) {
+    else if (target.innerHTML.includes("PRC117-") || target.innerHTML.includes("PRC152-"))
+    {
         malla = target.innerHTML.replace("PRC152-", "PRC117-");
-        for (let i = 0; i < allTds.length; i++) {
-            if (allTds[i].innerHTML == malla) {
+        for (let i = 0; i < allTds.length; i++)
+        {
+            if (allTds[i].innerHTML == malla && !allTds[i].matches(".disabled tr td"))
+            {
                 allTds[i].style.backgroundColor = "#40B0A6";
                 allTds[i].parentElement.parentElement.parentElement.parentElement.style.backgroundColor = "#40B0A6";
             }
         }
 
         malla = target.innerHTML.replace("PRC117-", "PRC152-");
-        for (let i = 0; i < allTds.length; i++) {
-            if (allTds[i].innerHTML == malla) {
+        for (let i = 0; i < allTds.length; i++)
+        {
+            if (allTds[i].innerHTML == malla && !allTds[i].matches(".disabled tr td"))
+            {
                 allTds[i].style.backgroundColor = "#40B0A6";
                 allTds[i].parentElement.parentElement.parentElement.parentElement.style.backgroundColor = "#40B0A6";
             }
@@ -40,8 +51,10 @@ function highlightThis(e) {
 
 }
 
-function unHighlightAll() {
-    for (let i = 0; i < allTds.length; i++) {
+function unHighlightAll()
+{
+    for (let i = 0; i < allTds.length; i++)
+    {
         allTds[i].style.backgroundColor = "";
     }
 }
